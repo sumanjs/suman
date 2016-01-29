033c93ac00a287fa4e4ea347ad508ae6197f8a7c
Alex Mills <alexmillsmail@gmail.com>
2016-01-28 19:33:18 -0800

    temp

--- test/integration-tests/test0.js
+++ test/integration-tests/test0.js
@@ -7,29 +7,29 @@ var debug = require('debug')('suman');
 var Test = require('../../lib').Test(module, 'suman.conf.js');
 
 
-Test.describe('gggg', {parallel: true}, function () {
+Test.describe('gggg', {parallel: false}, function () {
 
     this.describe('moodle', {
-        parallel: true
+        parallel: false
     }, function () {
 
         this.before(done => {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
 
         });
 
         this.before(done => {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.before(done => {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.after(function () {
@@ -38,35 +38,35 @@ Test.describe('gggg', {parallel: true}, function () {
 
 
     this.describe('moodle', {
-        parallel: true
+        parallel: false
     }, function () {
 
         this.beforeEach((t, done) => {
             setTimeout(function () {
                 debug('before Each 1, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.beforeEach((t, done)=> {
             setTimeout(function () {
                 debug('before Each 2, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.beforeEach((t, done) => {
             setTimeout(function () {
                 debug('before Each 3, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.it('mmm1', {parallel: false}, (t, done) => {
 
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
 
         });
 
@@ -74,7 +74,7 @@ Test.describe('gggg', {parallel: true}, function () {
 
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
 
         });
 
@@ -82,7 +82,7 @@ Test.describe('gggg', {parallel: true}, function () {
 
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
 
         });
 
@@ -90,21 +90,21 @@ Test.describe('gggg', {parallel: true}, function () {
             setTimeout(function () {
                 debug('before Each 4, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.afterEach(function (t, done) {
             setTimeout(function () {
                 debug('after Each 1, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.afterEach(function (t, done) {
             setTimeout(function () {
                 debug('after Each 2, ' + t.desc);
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.after(function () {
@@ -115,41 +115,41 @@ Test.describe('gggg', {parallel: true}, function () {
     });
 
 
-    this.describe('bum', {parallel: true}, function () {
+    this.describe('bum', {parallel: false}, function () {
 
 
         this.it('aaa1', {
-            parallel: true
+            parallel: false
         }, function (t, done) {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
         });
 
 
         this.it('aaa2', {
-            parallel: true
+            parallel: false
         }, function (t, done) {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
         });
 
 
         this.it('aaa3', {
-            parallel: true
+            parallel: false
         }, function (t, done) {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
         });
 
         this.it('aaa4', {
-            parallel: true
+            parallel: false
         }, function (t, done) {
             setTimeout(function () {
                 done();
-            }, 200);
+            }, 700);
 
         });
 
