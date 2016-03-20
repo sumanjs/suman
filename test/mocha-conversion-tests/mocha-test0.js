b1b9fd28f44a89493d7a04c15e14ec4ca74dc252
Alexander Mills <alexander_mills2@cable.comcast.com>
2016-03-20 01:59:23 -0700

    Well tested conversion kit in the works

---
+++ test/mocha-conversion-tests/mocha-test0.js
@@ -0,0 +1,19 @@
+/**
+ * Created by amills001c on 3/20/16.
+ */
+
+var assert = require('assert');
+
+describe('a',function(){
+
+    it('a', function(done){
+
+
+    });
+
+    it('a', function(){
+
+    });
+
+
+});
