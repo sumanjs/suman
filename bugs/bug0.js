96dd496265f6e69e3695ca59481ae11076c5bb51
Alexander Mills <alexander_mills2@cable.comcast.com>
2016-03-29 23:14:07 -0700

    Fixed async process.nextTick issue

---
+++ bugs/bug0.js
@@ -0,0 +1,38 @@
+/**
+ * Created by amills001c on 3/29/16.
+ */
+
+
+/**
+ * Created by rpowar001c on 3/29/16.
+ */
+
+const Test = require('../lib').init(module, {
+    export: false, //module.exports.wait = false;
+    integrants: []
+});
+
+
+Test.describe('BBB', {parallel: false}, function (fs) {
+
+    // console.log('fs:',fs);
+
+    this.before(function () {
+
+        // throw new Error('Yolo');
+    });
+
+
+    this.it('testing1', function () {
+
+        throw new Error('erroA');
+
+    }).it('testing2', function () {
+
+        console.log('testing2');
+
+    }).it('testing3', function () {
+        console.log('testing3');
+    });
+
+});
