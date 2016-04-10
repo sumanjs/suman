f109ed36c8c06eae6f189c81928ebbef244514a6
Alexander Mills <alexander_mills2@cable.comcast.com>
2016-04-10 02:35:05 -0700

    Fixing files that are run directly

--- config/suman-constants.js
+++ config/suman-constants.js
@@ -38,7 +38,8 @@ module.exports = Object.freeze({
         INTEGRANT_VERIFICATION_FAILURE: 64,
         UNKNOWN_RUNNER_CHILD_PROCESS_STATE: 65,
         ERROR_IN_ROOT_SUITE: 66,
-        IOC_DEPS_ACQUISITION_ERROR: 67
+        IOC_DEPS_ACQUISITION_ERROR: 67,
+        EXPORT_TEST_BUT_RAN_TEST_FILE_DIRECTLY: 68
     },
     ERROR_MESSAGES: {
         INVALID_FUNCTION_TYPE_USAGE: 'You cannot use an arrow function with describe callbacks; however, you may use arrow functions everywhere else.\n' +
