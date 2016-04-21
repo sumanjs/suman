16e73f19b98031c72136529b52f197b2410af276
the1mills <alexander.d.mills@gmail.com>
2016-04-21 02:07:49 -0700

    temp

--- test/build-tests/tes-t7.jsx
+++ test/build-tests/tes-t7.jsx
@@ -5,8 +5,8 @@
 
 
 const Test = require('../../lib').init(module, {
-    export: true,
-    integrants: ['smartconnect', 'dolce-vida'],
+    export: false,
+    //integrants: ['smartconnect', 'dolce-vida'],
     ioc: {
         choodles: function(){
             
@@ -15,116 +15,128 @@ const Test = require('../../lib').init(module, {
 });
 
 
-Test.describe('Suite7', {parallel: true}, function (fs, extra, choodles) {
+const d = require('domain').create();
+
+d.on('error', function(err){
+
+    console.error(err.stack);
+});
+
+d.run(function(){
+    Test.describe('Suite7', {parallel: true}, function (fs, extra, choodles) {
 
 
-    console.log('extra:',extra);
+        console.log('extra:',extra);
 
 
-    this.before(ctn => {
-        ctn();
-    });
+        this.before(ctn => {
+            ctn();
+        });
 
-    this.beforeEach(t => {
-        
+        this.beforeEach(t => {
 
-    });
-
-    this.describe('loop', function () {
-
-        [1, 2, 3, 4, 5, 6].forEach(val=> {
-
-            this.it('tests ' + val, {parallel: !!val}, function () {
-                
-                
-            });
-        })
-
-    });
-
-
-    this.describe('1', {efa: true}, function () {
-
-        this.before((done) => {
-
-            setTimeout(function () {
-                done();
-            }, 10);
 
         });
 
-        this.it('[test] yo 1', {parallel: true}, (t, fail, done, pass) => {
+        this.describe('loop', function () {
 
-            fs.createReadStream('/dev/null').pipe(fs.createWriteStream('/dev/null')).on('error', fail).on('finish', pass);
+            [1, 2, 3, 4, 5, 6].forEach(val=> {
+
+                this.it('tests ' + val, {parallel: !!val}, function () {
+
+
+                });
+            })
 
         });
 
 
-        this.it('[test] yo 2', {parallel: false}, function (t) {
+        this.describe('1', {efa: true}, function () {
 
-            return new Promise(function (resolve, reject) {
+            this.before((done) => {
 
-                resolve();
+                setTimeout(function () {
+                    done();
+                }, 10);
 
             });
 
-        });
+            this.it('[test] yo 1', {parallel: true}, (t, fail, done, pass) => {
 
-        //this.it('[test] yo 2', {parallel: false}, new Promise(function (resolve, reject) {
-        //
-        //    Promise.delay(1000).then(function () {
-        //        resolve();
-        //    });
-        //
-        //}).then(function(){
-        //
-        //
-        //
-        //}));
+                fs.createReadStream('/dev/null').pipe(fs.createWriteStream('/dev/null')).on('error', fail).on('finish', pass);
 
-
-        function p(val) {
-            return new Promise(function (resolve) {
-                resolve('doooog' + val);
             });
-        }
 
-        this.it('[test] gen', {parallel: false}, function *() {
 
-            var t = yield 3;
-            var val = yield p();
-            val = yield p(val);
+            this.it('[test] yo 2', {parallel: false}, function (t) {
+
+                return new Promise(function (resolve, reject) {
+
+                    resolve();
+
+                });
+
+            });
+
+            //this.it('[test] yo 2', {parallel: false}, new Promise(function (resolve, reject) {
+            //
+            //    Promise.delay(1000).then(function () {
+            //        resolve();
+            //    });
+            //
+            //}).then(function(){
+            //
+            //
+            //
+            //}));
+
+
+            function p(val) {
+                return new Promise(function (resolve) {
+                    resolve('doooog' + val);
+                });
+            }
+
+            this.it('[test] gen', {parallel: false}, function *() {
+
+                var t = yield 3;
+                var val = yield p();
+                val = yield p(val);
+
+            });
+
+
+            this.it('yo', {parallel: false}, (t, done) => {
+
+                // throw new Error('PAsta');
+                setTimeout(function () {
+
+                    done();
+                }, 100);
+
+            });
+
+            this.it('chubs', {parallel: false}, (t, done) => {
+
+                setTimeout(function () {
+                    done();
+                }, 100);
+
+            });
 
         });
 
 
-        this.it('yo', {parallel: false}, (t, done) => {
-
-            // throw new Error('PAsta');
-            setTimeout(function () {
-
-                done();
-            }, 100);
+        this.before(() => {
 
         });
 
-        this.it('chubs', {parallel: false}, (t, done) => {
-
-            setTimeout(function () {
-                done();
-            }, 100);
-
-        });
 
     });
-
-
-    this.before(() => {
-
-    });
-
 
 });
+
+
 
 
 /*
