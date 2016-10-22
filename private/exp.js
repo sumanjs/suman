3dc070dd99545ca0021d80b6a468da8f94894995
Alexander Mills <alexander.mills@autodesk.com>
2016-10-21 18:23:36 -0700

    publish/release:

--- private/exp.js
+++ private/exp.js
@@ -1,36 +1,10 @@
-// const Threads = require('webworker-threads');
-//
-// var thread = Threads.create();
-//
-// const roodles = 3;
-//
-// thread.eval(function () {
-//
-//     console.log('roodles:', roodles);
-// });
-//
 
 
-// console.log(Object.map);
+var str = /\.test\.js$/.toString();
 
+str = str.slice(1,-1);
 
-Object.map = function(obj, fn){
+console.log(str);
 
-    const ret = {};
+console.log(new RegExp(str));
 
-    Object.keys(obj).forEach(function(key){
-        ret[key] = fn.apply(null,[key]);
-    });
-
-    return ret;
-};
-
-
-const obj = {"a":5,"b":6};
-
-const newObject = Object.map(obj, key => {
-    return obj[key] + 5;
-});
-
-console.log(obj); // {"a":5,"b":6}
-console.log(newObject); // {"a":10,"b":11}
