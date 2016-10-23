613fefa724c00f2d8a16304ca34f6381f96cee14
Alexander Mills <alexander.mills@autodesk.com>
2016-10-22 23:08:38 -0700

    publish/release:

--- publish-suman.sh
+++ publish-suman.sh
@@ -14,17 +14,17 @@ git add . &&
 git add -A &&
 git commit -am "publish/release:${GIT_COMMIT_MSG}" &&
 git push &&
-git checkout -b dev_temp
+git checkout -b devtemp
 npm run remove-private-dirs &&
 npm run remove-private-files &&
 git add . &&
 git add -A &&
 git commit -am "publish/release:${GIT_COMMIT_MSG}" &&
 git remote add public git@github.com:ORESoftware/suman.git
-git fetch public &&
+git fetch --all public &&
 # git checkout -b master
 git checkout -b temp public/master &&
-git merge -Xtheirs --squash -m "squashed with dev" dev_temp
+git merge --squash -m "squashed with devtemp" devtemp
 npm run remove-private-dirs &&
 npm run remove-private-files &&
 git add . &&
@@ -33,7 +33,7 @@ git commit -am "publish/release:${GIT_COMMIT_MSG}" &&
 git push public temp:master &&
 git remote rm public &&
 git checkout dev &&
-git branch -D dev_temp &&
+git branch -D devtemp &&
 git branch -D temp &&
 npm publish .
 
