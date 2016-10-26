46c8558fc3047ad1e55118f63e6c4b73c7415e4f
Alexander Mills <alexander.mills@autodesk.com>
2016-10-26 12:09:15 -0700

    publish/release:

--- delete-internal-paths.sh
+++ delete-internal-paths.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # remove dirs
-git rm -r --ignore-unmatch private bugs examples file-examples internal-docs jsdoc-out node_modules .DS_Store
+git rm -r --ignore-unmatch public private bugs examples file-examples internal-docs jsdoc-out node_modules .DS_Store
 
 # remove files
 git rm --ignore-unmatch publish-gh-pages.sh pre-publish-gh-pages.sh publish-suman.sh exp*.js jsdoc-notes.txt suman-todos.txt .DS_Store
