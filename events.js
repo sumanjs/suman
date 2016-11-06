2d9fde5d7647019f096a59b2db182a4289f0a0fa
oresoftware <alex@oresoftware.com>
2016-11-06 14:59:19 -0800

    Not sure why working but error messaging is

--- events.js
+++ events.js
@@ -11,7 +11,6 @@ function makeToString (val) {
 
 const events = module.exports = Object.freeze({
 
-
   // runner events
   TEST_FILE_CHILD_PROCESS_EXITED: {
     explanation: 'runner is started, fires before any test child processes are started.',
--- private/exp.js
+++ private/exp.js
@@ -1,40 +1,35 @@
-//
 
-const _ = require('lodash');
+// var str = /\.test\.js$/.toString();
+//
+// str = str.slice(1,-1);
+//
+// console.log(str);
+//
+// console.log(new RegExp(str));
+
+function Proto() {
 
 
-console.log(_.flattenDeep('mother'));
-// // var str = /\.test\.js$/.toString();
-// //
-// // str = str.slice(1,-1);
-// //
-// // console.log(str);
-// //
-// // console.log(new RegExp(str));
-//
-// function Proto() {
-//
-//
-// }
-//
-// Proto.prototype = Object.create(Function.prototype);
-//
-//
-// Proto.prototype.foo = function () {
-//     console.log('foo');
-// };
-//
-// const p = new Proto();
-//
-//
-// const f1 = function () {
-//     console.log('f');
-// };
-//
-//
-// console.log('typeof f before => ', typeof f1);
-//
-// const f2 = Object.setPrototypeOf(f1, p);
-//
-//
-// console.log('typeof f after => ', typeof f2);
+}
+
+Proto.prototype = Object.create(Function.prototype);
+
+
+Proto.prototype.foo = function () {
+    console.log('foo');
+};
+
+const p = new Proto();
+
+
+const f1 = function () {
+    console.log('f');
+};
+
+
+console.log('typeof f before => ', typeof f1);
+
+const f2 = Object.setPrototypeOf(f1, p);
+
+
+console.log('typeof f after => ', typeof f2);
