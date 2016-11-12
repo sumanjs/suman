8ff8012f7a5ec97972c8ceace17443d0503658da
oresoftware <alex@oresoftware.com>
2016-11-11 23:13:38 -0800

    node

--- index-troubleshoot.js
+++ index-troubleshoot.js
@@ -1,5 +1,12 @@
-/**
- * Created by Olegzandr on 11/11/16.
- */
+#!/usr/bin/env node --harmony
 
- // suman test.js | suman-troubleshoot
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (String(chunk).match('suman')) {
+    process.stdout.write(chunk);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
