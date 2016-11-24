bf07f78c813298fc633d0a3820f3804530b87a6c
oresoftware <alex@oresoftware.com>
2016-11-23 19:07:21 -0800

    publish/release:foo

--- private/exp9.js
+++ private/exp9.js
@@ -1,3 +1,24 @@
+function log (d) {
 
+  if (!String(d).match(/npm info/ig) && !String(d).match(/npm http/ig)) {
+    console.log(d);
+    // process.stderr.write(String(d));
+  }
+}
 
-console.log(process.env.HOME);
+const foo = 'abc\ndef\nae\nz';
+const lines = String(foo).split('\n');
+
+var line = 'npm info';
+
+line += lines.shift();
+
+log('line 1 => ' + line);
+
+for (var i = 0; i < lines.length - 1; i++) {
+  log(lines[ i ]);
+}
+
+line = lines[ i ];
+
+log('line 2 => ' + line);
--- scripts/a.js
+++ scripts/a.js
@@ -1,3 +1,49 @@
 
 
-console.log('postinstall all good!');
+console.log(' all good!');
+console.error(' is error!');
+
+At one point I thought you could tell Node.js child process to chunk the data by newline character.
+
+  I have this:
+
+
+
+const sh = spawn('sh', [ b ], {
+  cwd: cwd,
+});
+
+sh.stdout.pipe(fs.createWriteStream('/dev/null'));
+
+var stderr = '';
+var line = '';
+
+sh.stderr.setEncoding('utf8');
+
+sh.stderr.on('data', function (d) {
+
+  //trying to split by newlines, but this is hairy logic
+  const lines = String(d).split('\n');
+
+  line += lines.shift();
+
+  if(lines.length > 0){
+
+    if (!String(d).match(/npm/ig) && !String(d).match(/npm/ig)) {
+      stderr += d;
+      process.stderr.write.apply(process.stderr, arguments);
+    }
+
+  }
+
+});
+
+and the data come back in this handler is not whole lines
+
+sh.stderr.on('data', function (d) {
+  // d is chunks of data but not whole lines
+});
+
+isn't there a way to tell stderr to wait for newline chars before firing the 'data' event?
+
+
