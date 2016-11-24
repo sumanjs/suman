24394b46575927903879557229615d91e2a55abc
oresoftware <alex@oresoftware.com>
2016-11-23 19:08:51 -0800

    dahm

--- private/exp9.js
+++ private/exp9.js
@@ -1,5 +1,24 @@
-var a = [];
+function log (d) {
 
-const b = a.slice(0);
+  if (!String(d).match(/npm info/ig) && !String(d).match(/npm http/ig)) {
+    console.log(d);
+    // process.stderr.write(String(d));
+  }
+}
 
-console.log(process.env.HOME);
+const foo = 'abc\ndef\nae\nz';
+const lines = String(foo).split('\n');
+
+var line = 'npm info';
+
+line += lines.shift();
+
+log('line 1 => ' + line);
+
+for (var i = 0; i < lines.length - 1; i++) {
+  log(lines[ i ]);
+}
+
+line = lines[ i ];
+
+log('line 2 => ' + line);
