5a7c9a382590fe711274908015af230010e0576b
oresoftware <alex@oresoftware.com>
2016-11-30 22:13:53 -0800

    publish/release:debug

--- private/exp6.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-const p1 = path.normalize('/test/a/b/c/');
-const p2 = path.normalize('/test/a/b/c');
-
-console.log(p1, p2);
--- private/foo.js
+++
@@ -1,2 +0,0 @@
-const arr = [1,2,3];
-console.log(arr.concat(4));
