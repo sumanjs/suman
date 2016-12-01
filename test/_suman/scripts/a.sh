0158775c0befeb90fe22ba4c41aeb8b9e7b4d3b7
oresoftware <alex@oresoftware.com>
2016-11-30 21:34:37 -0800

    publish/release:init

--- test/_suman/scripts/a.sh
+++ test/_suman/scripts/a.sh
@@ -5,8 +5,11 @@ echo ${PWD} &&
 mkdir project &&
 cd project &&
 npm init -f &&
+echo "Installign suman with 'npm install -D --silent github:oresoftware/suman' " &&
 npm install -D --silent github:oresoftware/suman &&
+echo "Making test directory" &&
 mkdir test &&
+
 ./node_modules/.bin/suman --init &&
 ./node_modules/.bin/suman --create test/one.test.js &&
 ./node_modules/.bin/suman &&
