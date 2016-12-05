e29d531ad87f88f8ced3a748d1c170637acaeb4b
Olegzandr Denman <alex@oresoftware.com>
2016-12-04 19:25:37 -0800

    log

--- scripts/suman-postinstall.sh
+++ scripts/suman-postinstall.sh
@@ -3,13 +3,17 @@
 ./scripts/create-suman-dir.js &&
 SUMAN_CONF_JS=$(dirname $(dirname $PWD))/suman.conf.js
 
+LOG_PATH=~/.suman/suman-debug.log
+
 echo " => Potential suman.conf.js file path => ${SUMAN_CONF_JS}"
 
 # if suman.conf.js exists, then we run things in "foreground", otherwise run as daemon
 if [ -e "$SUMAN_CONF_JS" ]; then
+    echo " => suman.conf.js file found."
     ./scripts/install-suman-home.sh &&
     ./scripts/on-install-success.js
 else
-    ./scripts/install-suman-home.sh & echo " => Suman optional deps being installed as daemon.\n"
+    echo " => suman.conf.js file *not* found."
+    ./scripts/install-suman-home.sh > ${LOG_PATH} 2>&1 & echo " => Suman optional deps being installed as daemon.\n"
 fi
 
