c9f931dd87c6dcba8fec968d80ea8ac08bacf5e4
Olegzandr Denman <alex@oresoftware.com>
2016-12-07 16:47:17 -0800

    reset paths

--- delete-internal-paths.sh
+++ delete-internal-paths.sh
@@ -5,7 +5,7 @@ git rm -r --ignore-unmatch .idea public private bugs examples internal-docs \
 jsdoc-out node_modules .DS_Store &&
 
 # remove files
-git rm --ignore-unmatch jsdoc.conf.json docker-notes.txt publish-gh-pages.sh pre-publish-gh-pages.sh \
+git rm --ignore-unmatch pdev.sh build.sh docker-notes.txt jsdoc.conf.json docker-notes.txt publish-gh-pages.sh pre-publish-gh-pages.sh \
 publish-suman*.sh exp*.js jsdoc-notes.txt suman-todos.txt .DS_Store &&
 
 # all done
