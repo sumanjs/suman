a57799c9164e90b07a75790f960ad8da4c0ef131
Olegzandr Denman <alex@oresoftware.com>
2016-12-18 20:24:52 -0800

    pdev:set

--- scripts/create-suman-dir.js
+++ scripts/create-suman-dir.js
@@ -22,20 +22,17 @@ const findSumanExec = path.resolve(p + '/find-local-suman-executable.js');
 const sumanClis = path.resolve(p + '/suman-clis.sh');
 const findProjectRootDest = path.resolve(p + '/find-project-root.js');
 const sumanDebugLog = path.resolve(p + '/suman-debug.log');
-const sumanClisFile = fs.readFileSync(require.resolve('./suman-clis.sh'));
-const findSumanExecFile = fs.readFileSync(require.resolve('./find-local-suman-executable.js'));
-const findProjectRoot = fs.readFileSync(require.resolve('./find-project-root.js'));
+
+///////////////////////////////////////////////////
 const sumanHome = path.resolve(process.env.HOME + '/.suman');
 const queue = path.resolve(process.env.HOME + '/.suman/install-queue.txt');
-
-const debug = require('suman-debug');
-const debugPostinstall = debug('s:postinstall');
+const debug = require('suman-debug')('s:postinstall');
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-debugPostinstall(' => In Suman postinstall script, cwd => ', cwd);
-debugPostinstall(' => In Suman postinstall script => ', __filename);
-debugPostinstall(' => Suman home dir path => ', p);
+debug(' => In Suman postinstall script, cwd => ', cwd);
+debug(' => In Suman postinstall script => ', __filename);
+debug(' => Suman home dir path => ', p);
 
 fs.mkdir(p, function (err) {
 
@@ -43,17 +40,33 @@ fs.mkdir(p, function (err) {
         throw err;
     }
 
-    debugPostinstall(' => Beginning of Suman post-install script');
+    debug(' => Beginning of Suman post-install script');
 
     async.parallel([
 
         function (cb) {
             //always want to update this file to the latest version, so always overwrite
-            fs.writeFile(sumanClis, sumanClisFile, {flag: 'w', flags: 'w'}, cb);
+            fs.readFile(require.resolve('./suman-clis.sh'), function (err, data) {
+                if (err) {
+                    cb(err);
+                }
+                else {
+                    fs.writeFile(sumanClis, data, {flag: 'w', flags: 'w'}, cb);
+                }
+            });
+
         },
         function (cb) {
             //always want to update this file to the latest version, so always overwrite
-            fs.writeFile(findSumanExec, findSumanExecFile, {flag: 'w', flags: 'w'}, cb);
+            fs.readFile(require.resolve('./find-local-suman-executable.js'), function (err, data) {
+                if (err) {
+                    cb(err);
+                }
+                else {
+                    fs.writeFile(findSumanExec, data, {flag: 'w', flags: 'w'}, cb);
+                }
+            });
+
         },
         function (cb) {
             fs.writeFile(sumanDebugLog, '\n\n => Suman post-install script run on ' + new Date()
@@ -64,7 +77,15 @@ fs.mkdir(p, function (err) {
             fs.writeFile(queue, '', {flag: 'a', flags: 'a'}, cb);
         },
         function (cb) {
-            fs.writeFile(findProjectRootDest, findProjectRoot, {flag: 'w', flags: 'w'}, cb);
+            fs.readFile(require.resolve('./find-project-root.js'), function (err, data) {
+                if (err) {
+                    cb(err);
+                }
+                else {
+                    fs.writeFile(findProjectRootDest, data, {flag: 'w', flags: 'w'}, cb);
+                }
+            });
+
         }
 
 
@@ -78,11 +99,11 @@ fs.mkdir(p, function (err) {
         else {
 
             if (fs.existsSync(sumanHome)) {
-                debugPostinstall(' => ~/.suman dir exists!');
+                debug(' => ~/.suman dir exists!');
                 process.exit(0);
             }
             else {
-                debugPostinstall(' => Warning => ~/.suman dir does not exist!');
+                debug(' => Warning => ~/.suman dir does not exist!');
                 process.exit(1)
             }
 
