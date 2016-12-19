786959085e6d8494801997ee75947b3d3a428e16
Olegzandr Denman <alex@oresoftware.com>
2016-12-18 21:34:47 -0800

    pdev:set

--- test/testsrc/fly-user/open-subl.js
+++ test/testsrc/fly-user/open-subl.js
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env node
 
 
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
-
 const dir = path.resolve(process.env.HOME + '/suman-test/suman-test-projects/subprojects');
 
 const items = fs.readdirSync(dir).map(function (item) {
