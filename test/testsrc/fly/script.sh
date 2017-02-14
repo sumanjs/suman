999e5071cd0a36591241dcc154fb86a33db4dbc7
Olegzandr Denman <alex@oresoftware.com>
2017-02-13 23:23:06 -0800

    tests

--- test/testsrc/fly/script.sh
+++ test/testsrc/fly/script.sh
@@ -8,7 +8,7 @@ mkdir suman-test
 cd suman-test &&
 rm -rf suman-test-projects &&
 #git clone git@github.com:sumanjs/suman-test-projects.git &&
-git clone https://github.com/sumanjs/suman-test-projects.git &&
+git clone --depth 1 https://github.com/sumanjs/suman-test-projects.git &&
 cd suman-test-projects &&
 git checkout master &&
 ./test/test-all.sh &&
