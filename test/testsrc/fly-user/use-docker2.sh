04a3076e73b975c20c14144be92877239d1698e8
Olegzandr Denman <alex@oresoftware.com>
2017-02-13 20:04:32 -0800

    pdev:push

--- test/testsrc/fly-user/use-docker2.sh
+++ test/testsrc/fly-user/use-docker2.sh
@@ -3,6 +3,7 @@
 ############################################################
 
 MY_DIR=$(cd $(dirname "$0") && pwd)
+docker stop $(docker ps -a -q) -t 1 # stop all running containers
 
 export USE_DOCKER=yes
 export SUMAN_DEBUG=s
@@ -15,11 +16,9 @@ git clone https://github.com/sumanjs/suman-test-projects.git &&
 cd suman-test-projects &&
 #git checkout -b test_branch &&
 echo "installing suman deps locally"
-SUMAN_POSTINSTALL_IS_DAEMON=no npm install --progress=false --loglevel=warn &>/dev/null &&
+SUMAN_POSTINSTALL_IS_DAEMON=yes npm install --progress=false --loglevel=warn &>/dev/null &&
 echo "args => $@"
-suman --concurrency=6 --groups $@
-
-##############################################################
+suman --concurrency=9 --groups $@
 
 EXIT=$?
 echo " => bash exit code for script '$(dirname "$0")/$(basename "$0")' => $EXIT" &&
