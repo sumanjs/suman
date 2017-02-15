cd18dfdecb9f5a300fe3abdcf4f8c000ba2d6525
Olegzandr Denman <alex@oresoftware.com>
2017-02-15 02:59:36 -0800

    pdev:push

--- test/testsrc/shell/parallel.sh
+++ test/testsrc/shell/parallel.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+set -m # allow for job control
+EXIT_CODE=0;  # exit code of overall script
 
-echo "x" & X=$!
-echo "y" & Y=$!
-(  echo "z" && exit 1; ) & Z=$!;
+function handleJobs() {
+     for job in `jobs -p`; do
+         echo "PID => ${job}"
+#        if  ! wait ${job} ; then
+         CODE=0;
+         wait ${job} || CODE=$?
+         if [[ "${CODE}" != "0" ]]; then
+         echo "At least one test failed with exit code => ${CODE}" ;
+         EXIT_CODE=1;
+         fi
+     done
+}
 
+trap 'handleJobs' CHLD
+DIRN=$(dirname "$0");
 
-wait ${Z} ; echo "Z $?" # returns exit code given the PID
-wait ${X} ; echo "X $?" # returns exit code given the PID
-wait ${Y} ; echo "Y $?" # returns exit code given the PID
+commands=(
+   "echo $PWD && suman test/testsrc/es5-es6"
+   "echo $PWD && suman test/testsrc/es5-es6"
+   "echo $PWD && suman test/testsrc/es5-es6"
+)
 
+clen=`expr "${#commands[@]}" - 1` # get length of commands - 1
+
+for i in `seq 0 "$clen"`; do
+    (echo "${commands[$i]}" | bash) &   # run the command via bash in subshell
+    echo "$i ith command has been issued as a background job"
+done
+
+# wait for all to finish
+wait;
+
+echo "EXIT_CODE => $EXIT_CODE"
+exit "$EXIT_CODE"
