60e49b2159c797e1d5d9c076b5887bb0a92fcaaa
Olegzandr Denman <alex@oresoftware.com>
2017-02-15 22:36:58 -0800

    runner-refactor

--- private/exp3.js
+++ private/exp3.js
@@ -1,36 +1,12 @@
 
+const c= function mmm(){
 
-"use strict";
+};
 
-Object.freezeExistingProps = function(obj, modifyProto){
-
-    Object.keys(obj).forEach(function(key){
-
-        const val = obj[key];
-
-        Object.defineProperty(obj, key , {  //we simply overwrite existing prop
-            value: val,
-            writable: false, // important, I think
-            enumerable: true,
-            configurable: false
-        });
-
-
-    });
-
-    return obj;
+const aaaa = t => {
 
 };
 
 
-var z = {
-    foo: 'bar'
-};
-
-var k = Object.freezeExistingProps(z);
-
-k.foo = 'mmmm';
-
-console.log(z);
-console.log(k);
-
+console.log(c.name);
+console.log(aaaa.name);
