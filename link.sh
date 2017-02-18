5bdd5783548b7adb8f850efd36e3dd5c12faa29b
oresoftware <alex@oresoftware.com>
2017-02-17 23:26:55 -0800

    pdev

--- link.sh
+++ link.sh
@@ -6,10 +6,17 @@
 cd $(dirname "$0")
 rm -rf node_modules
 npm install
+
+# link dev deps
 npm link suman-utils -f
 npm link suman-debug -f
 npm link suman-events -f
 npm link suman-example-reporter -f
+
+# link peer deps
+npm link suman-server -f
+npm link suman-inquirer -f
+npm link suman-inquirer-directory -f 
 
 # finally we link suman
 npm link .
