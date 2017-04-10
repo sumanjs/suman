70bca07ea6e105558ebd88abb3490361e9b31537
alexamil <alex@oresoftware.com>
2017-04-09 21:36:24 -0700

    merge master

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,44 +1,38 @@
 /// <reference types="node" />
-declare namespace suman {
-    interface ILoadOpts {
-        path: string;
-        indirect: boolean;
-    }
-    interface Ioc {
-        a: string;
-        b: string;
-    }
-    interface IInitOpts {
-        export?: boolean;
-        __expectedExitCode?: number;
-        pre?: Array<string>;
-        integrants?: Array<string>;
-        series?: boolean;
-        writable?: boolean;
-        timeout?: number;
-        post?: Array<any>;
-        interface?: string;
-        iocData?: Object;
-        ioc?: Object;
-    }
-    interface ICreate {
-        create: Function;
-    }
-    interface IInit {
-        (module: NodeModule, opts: IInitOpts): ICreate;
-        $ingletonian?: any;
-        tooLate?: boolean;
-    }
-    interface IInitExport {
-        load: Function;
-        autoPass: Function;
-        autoFail: Function;
-        init: IInit;
-        constants: Object;
-        Writable: Function;
-        Transform: Function;
-        once: Function;
-    }
+export interface Ioc {
+    a: string;
+    b: string;
 }
-declare const suman: suman.IInitExport;
-export = suman;
+export interface IInitOpts {
+    export?: boolean;
+    __expectedExitCode?: number;
+    pre?: Array<string>;
+    integrants?: Array<string>;
+    series?: boolean;
+    writable?: boolean;
+    timeout?: number;
+    post?: Array<any>;
+    interface?: string;
+    iocData?: Object;
+    ioc?: Object;
+}
+export interface ICreate {
+    create: Function;
+}
+export interface IInit {
+    (module: NodeModule, opts: IInitOpts): ICreate;
+    $ingletonian?: any;
+    tooLate?: boolean;
+}
+export interface IInitExport {
+    load: Function;
+    autoPass: Function;
+    autoFail: Function;
+    init: IInit;
+    constants: Object;
+    Writable: Function;
+    Transform: Function;
+    once: Function;
+}
+declare const indexExports: IInitExport;
+export = indexExports;
