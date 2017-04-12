ce337bb189f3554f0c291fd84d43fc1bd6244d66
alexamil <alex@oresoftware.com>
2017-04-11 17:50:43 -0700

    vers

--- cli.js
+++ cli.js
@@ -168,6 +168,7 @@ if (!projectRoot) {
 ////////////////////////////////////////////////////////////////////
 
 const sumanOpts = _suman.sumanOpts = require('./lib/parse-cmd-line-opts/parse-opts');
+console.log('!! clliiiii');
 _suman.sumanArgs = sumanOpts._args;
 
 if (sumanOpts.verbose) {
