67dc168422ac6d8dc45289e3c93c825f88156958
Olegzandr VD <alex@oresoftware.com>
2017-07-24 01:22:44 -0700

    final commit before rebase 3

---
+++ lib/cli-commands/suman-daemon/index1.d.ts
@@ -0,0 +1,3 @@
+export declare const run: (projectRoot: string, sumanLibRoot: string, cb: Function) => any;
+export declare const run2: (projectRoot: string, cb: Function) => any;
+export declare const run3: (projectRoot: string, sumanLibRoot: string, cb: Function) => any;
