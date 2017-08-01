b2efd7834f6d3e67f276412ab31ff8ef3262d20c
Olegzandr VD <alex@oresoftware.com>
2017-08-01 14:33:30 -0700

    remove files

--- lib/reporters-Z/std-reporter.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import EventEmitter = NodeJS.EventEmitter;
-import { ISumanOpts } from "../../dts/global";
-declare const _default: (s: EventEmitter, sumanOpts: ISumanOpts) => void;
-export = _default;
