c6ec5ff39c92d74832b5d476af26228f085016d1
Operations Research Engineering Software+ <alex@oresoftware.com>
2017-08-01 12:54:44 -0700

    Rebase branch (#139) (#140)
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * pdev:set
    
    * final commit before rebase 1
    
    * pdev:set

---
+++ ∆merge-feature-branch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e; # exit immediately if any command fails
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$current_branch" == "master" || "$current_branch" == "dev" ]]; then
+    echo 'Aborting script because you are on master or dev branch, you need to be on a feature branch.';
+    exit 1;
+fi
+
+time_millis=$(node -e 'console.log(Date.now())');
+
+git add .
+git add -A
+git commit --allow-empty -am "feature:merge:${time_millis}"
+
+NEW_FEATURE_BRANCH="feature_${USER}_${time_millis}"
+
+git fetch origin
+git merge --no-ff origin/dev # use --no-ff to force a new commit
+git push origin HEAD
+
+git checkout dev
+git merge origin/dev
+
+git checkout -b "${NEW_FEATURE_BRANCH}"
+
+
+
+
