f883177d25818b09dfca5bf2828f0849c5b6ddbf
Olegzandr VD <alex@oresoftware.com>
2017-09-06 18:43:49 -0700

    pdev:set

--- lib/runner-helpers/handle-multiple-processes-old.d.ts
+++
@@ -1,6 +0,0 @@
-import { IRunnerObj, ISumanChildProcess, ITableRows } from "../../dts/runner";
-export interface ISumanCPMessages {
-    code: number;
-    signal: any;
-}
-export default function (runnerObj: IRunnerObj, tableRows: ITableRows, messages: Array<ISumanCPMessages>, forkedCPs: Array<ISumanChildProcess>, handleMessage: Function, beforeExitRunOncePost: Function, makeExit: Function): Function;
