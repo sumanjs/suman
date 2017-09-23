a65190a990c1c59ea2f46ada9d8f1ea407aff56b
Olegzandr VD <alex@oresoftware.com>
2017-09-22 20:42:41 -0700

    pdev:set

--- test/simple.js
+++ test/simple.js
@@ -8,7 +8,7 @@ const Test = suman.init(module);
 Test.create('hotels1', {parallel: false}, function (it, before, beforeEach) {
 
   it.cb('first', t => {
-    setTimeout(t, 2000);
+    setTimeout(t, 200);
   });
 
 });
@@ -17,10 +17,8 @@ Test.create('hotels1', {parallel: false}, function (it, before, beforeEach) {
 
 Test.create('hotels2', {parallel: false}, function (it, before, beforeEach) {
 
-  console.log('we in.');
-
   it.cb('second', t => {
-    setTimeout(t, 1000);
+    setTimeout(t, 100);
   });
 
 });
