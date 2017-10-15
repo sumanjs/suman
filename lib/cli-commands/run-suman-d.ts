320f5271070c1078ba7b33fbae3fc999207bc3cf
Olegzandr VD <alex@oresoftware.com>
2017-10-15 13:36:04 -0700

    pdev:set

--- lib/cli-commands/run-suman-d.ts
+++ lib/cli-commands/run-suman-d.ts
@@ -1,7 +1,10 @@
-import {startSumanD, ISubsetSumanDOptions} from 'suman-d';
+import {startSumanShell, ISubsetSumanDOptions} from 'suman-shell';
 
 export const run = function (projectRoot: string, sumanLibRoot: string, opts: ISubsetSumanDOptions) {
 
-  const fn = startSumanD(projectRoot, sumanLibRoot, opts || {});
+  const fn = startSumanShell(projectRoot, sumanLibRoot, opts || {});
+
+  // call clean up fn when process exits
+  process.once('exit', fn);
 
 };
