bbea99643fa318f6b98baab0087298d30fe8b02b
Olegzandr VD <alex@oresoftware.com>
2017-12-12 12:35:10 -0800

    pdev:set

--- lib/test-suite-helpers/handle-injections.ts
+++ lib/test-suite-helpers/handle-injections.ts
@@ -3,12 +3,14 @@
 //dts
 import {IInjectionObj, ITestSuite} from "suman-types/dts/test-suite";
 import {IPseudoError, IGlobalSumanObj} from "suman-types/dts/global";
+import {ErrorCallback, Dictionary} from 'async';
 
 //polyfills
 const process = require('suman-browser-polyfills/modules/process');
 const global = require('suman-browser-polyfills/modules/global');
 
 //core
+import assert = require('assert');
 import util = require('util');
 import path = require('path');
 
@@ -18,8 +20,8 @@ import {freezeExistingProps as freeze} from 'freeze-existing-props';
 import su = require('suman-utils');
 
 //project
-const _suman : IGlobalSumanObj = global.__suman = (global.__suman || {});
-import {tProto} from './t-proto';
+const _suman: IGlobalSumanObj = global.__suman = (global.__suman || {});
+import {makeInjectObj} from './t-proto-inject';
 import {constants} from "../../config/suman-constants";
 const weAreDebugging = su.weAreDebugging;
 
@@ -29,15 +31,22 @@ interface IInjectionRetObj {
   [key: string]: any
 }
 
+interface IInjectionValues {
+  k: string,
+  val: any
+}
+
 ///////////////////////////////////////////////////////////////////
 
-export const handleInjections = function (suite: ITestSuite, cb: Function) {
-
-  const addValuesToSuiteInjections = function (k: string, val: any) : void {
+export const handleInjections = function (suite: ITestSuite, cb: ErrorCallback<any>) {
+  
+  const addValuesToSuiteInjections = function (k: string, val: any): void {
     if (k in suite.injectedValues) {
       throw new Error(` => Injection value '${k}' was used more than once; this value needs to be unique.`);
     }
-
+    
+    console.log('registering key =>', k);
+    
     // freeze the property so it cannot be modified by user after the fact
     Object.defineProperty(suite.injectedValues, k, {
       enumerable: true,
@@ -46,21 +55,21 @@ export const handleInjections = function (suite: ITestSuite, cb: Function) {
       value: val
     });
   };
-
-  const isDescValid = function(desc: string){
+  
+  const isDescValid = function (desc: string) {
     return desc && String(desc) !== String(constants.UNKNOWN_INJECT_HOOK_NAME)
   };
-
+  
   const injections = suite.getInjections();
-
-  async.each(injections, function (inj: IInjectionObj, cb: Function) {
-
-    let callable = true;
-
+  
+  async.eachSeries(injections, function (inj: IInjectionObj, cb: Function) {
+    
+    let callable = true, timeoutVal = weAreDebugging ? 5000000 : inj.timeout;
+    
     const to = setTimeout(function () {
-      first(new Error(` => Injection hook timeout. ${inj.desc && 'For injection with name => ' + inj.desc}`));
-    }, weAreDebugging ? 5000000 : inj.timeout);
-
+      first(new Error(`Injection hook timeout. ${'For injection with name => ' + inj.desc}`));
+    }, timeoutVal);
+    
     const first = function (err: IPseudoError) {
       if (callable) {
         callable = false;
@@ -68,81 +77,71 @@ export const handleInjections = function (suite: ITestSuite, cb: Function) {
         process.nextTick(cb, err);
       }
       else if (err) {
-        console.error(' => Callback was called more than once, with error => \n', err.stack || err);
+        _suman.log.error('Callback was called more than once, with the following error:');
+        _suman.log.error(err.stack || err);
       }
     };
-
-    if (inj.cb) {
-
-      let d = function (err: IPseudoError, results: IInjectionRetObj) {
-
-        if (err) {
-          return first(err);
-        }
-
-        Promise.resolve(results).then(ret => {
-
-          if (isDescValid(inj.desc)) {
-            addValuesToSuiteInjections(String(inj.desc), ret);
+    
+    const values: Array<IInjectionValues> = [];
+    const assertCount = {num: 0};
+    
+    return new Promise(function (resolve, reject) {
+      
+      const injParam = makeInjectObj(inj, assertCount, suite, values, reject, resolve);
+      
+      if (inj.cb) {
+        
+        let d = function (err: IPseudoError, results: IInjectionRetObj) {
+          
+          if (err) {
+            return reject(err);
           }
-          else {
-            Object.keys(ret).forEach(function (k) {
-              addValuesToSuiteInjections(String(k), freeze(ret[k]));
-            });
-          }
-
-          first(undefined);
-
-        }, first);
-      };
-
-      inj.fn.call(suite, Object.setPrototypeOf(d, tProto));
-    }
-
-    else {
-
-      Promise.resolve(inj.fn.call(suite)).then(ret => {
-
-        if (isDescValid(inj.desc)) {
-          addValuesToSuiteInjections(inj.desc, freeze(ret));
-          return first(undefined);
-        }
-
-        if (typeof ret !== 'object') {
-          throw new Error('Must return an object with keys, if no inject hook name is provided.');
-        }
-
-        if (Array.isArray(ret)) {
-          throw new Error('Must return an object with named keys, if no inject hook name is provided.');
-        }
-
-        const keys = Object.keys(ret);
-
-        if (!keys.length) {
-          throw new Error('Injection hook was unnamed, but also resolved to object with no keys,\nso no name could' +
-            'be extracted for injection. Unfortunately this becomes fatal.');
-        }
-
-        const potentialPromises = keys.map(function (k) {
-          return ret[k];
-        });
-
-        Promise.all(potentialPromises).then(function (vals) {
-
-          keys.forEach(function (k, index) {
-            addValuesToSuiteInjections(String(k), freeze(vals[index]));
-          });
-
-          first(undefined);
-
-        }, first);
-
-      }, first);
-
-    }
-
+          
+          Promise.resolve(results).then(resolve, reject);
+        };
+        
+        inj.fn.call(null, Object.setPrototypeOf(d, injParam));
+      }
+      else {
+        
+        Promise.resolve(inj.fn.call(null, injParam))
+        .then(resolve, reject)
+        
+      }
+    })
+    .then(function () {
+      
+      // const keys = Object.keys(valuesMap);
+      // return Promise.all(keys.map(function (k) {
+      //   return valuesMap[k];
+      // }))
+      // .then(function (values) {
+      //
+      //   keys.forEach(function (k, i) {
+      //     addValuesToSuiteInjections(k, values[i]);
+      //   });
+      //
+      //   first(null);
+      //
+      // });
+      
+      const p = values.reduce(function (a, b) {
+        //run promises in series
+        return Promise.resolve(b.val)
+        .then(function (v) {
+          return addValuesToSuiteInjections(b.k, v);
+        })
+        
+      }, null);
+      
+      return p.then(function () {
+        first(null);
+      });
+    })
+    .catch(first);
+    
   }, cb);
-
+  
 };
 
 
--- lib/test-suite-helpers/t-proto-inject.ts
+++ lib/test-suite-helpers/t-proto-inject.ts
@@ -4,6 +4,8 @@
 import {IAssertObj, IHandleError, IHookObj, IHookParam} from "suman-types/dts/test-suite";
 import {IGlobalSumanObj} from "suman-types/dts/global";
 import AssertStatic = Chai.AssertStatic;
+import {ITestSuite} from 'suman-types/dts/test-suite';
+import {ErrorCallback, Dictionary} from 'async';
 
 //polyfills
 const process = require('suman-browser-polyfills/modules/process');
@@ -13,6 +15,10 @@ const global = require('suman-browser-polyfills/modules/global');
 import assert = require('assert');
 const chai = require('chai');
 const chaiAssert = chai.assert;
+
+//npm
+import su = require('suman-utils');
+import async = require('async');
 
 //project
 const _suman: IGlobalSumanObj = global.__suman = (global.__suman || {});
@@ -31,13 +37,15 @@ let badProps = <IBadProps> {
 
 /////////////////////////////////////////////////////////////////////////////////
 
-export const makeHookObj = function (hook: IHookObj, assertCount: IAssertObj,
+export const makeInjectObj = function (inject: IHookObj, assertCount: IAssertObj, suite: ITestSuite,
+                                       values: Array<any>,
                                      handleError: IHandleError, fini: Function): IHookParam {
 
   let planCalled = false;
   const v = Object.create(tProto);
+  const valuesMap = {} as any;
 
-  v.__hook = hook;
+  v.__hook = inject;
   v.__handle = v.__handleErr = handleError;
   v.__fini = fini;
 
@@ -48,6 +56,72 @@ export const makeHookObj = function (hook: IHookObj, assertCount: IAssertObj,
     catch (e) {
       return handleError(e);
     }
+  };
+  
+  
+  v.registerKey = v.register = function (k: string, val: any): Promise<any> {
+    assert(k && typeof k === 'string', 'key must be a string.');
+    {
+      if (k in valuesMap) {
+        throw new Error(`Injection key '${k}' has already been added.`);
+      }
+      if (k in suite.injectedValues) {
+        throw new Error(`Injection key '${k}' has already been added.`);
+      }
+    }
+    
+    valuesMap[k] = true; // mark
+    values.push({k, val});
+    return Promise.resolve(val);
+  };
+  
+  v.registerFnsMap = v.registerFnMap = function (o: Dictionary<any>): Promise<any> {
+    
+    assert(su.isObject(o), 'value must be a non-array object.');
+    
+    return new Promise(function (resolve, reject) {
+      async.series(o, function (err: Error, results: Dictionary<any>) {
+        
+        console.log('err => ', err);
+        console.log('results => ', results);
+        
+        if (err) {
+          return reject(err);
+        }
+        
+        Object.keys(results).forEach(function (k) {
+          if (k in valuesMap) {
+            return reject(new Error(`Injection key '${k}' has already been added.`));
+          }
+          if (k in suite.injectedValues) {
+            return reject(new Error(`Injection key '${k}' has already been added.`));
+          }
+          
+          valuesMap[k] = true;
+          values.push({k, val: results[k]});
+        });
+        
+        resolve(results);
+      });
+    });
+  };
+  
+  v.registerMap = v.registerPromisesMap = function (o: Dictionary<any>): Promise<Array<any>> {
+    
+    let keys = Object.keys(o);
+    return Promise.all(keys.map(function (k) {
+      
+      if (k in valuesMap) {
+        throw new Error(`Injection key '${k}' has already been added.`);
+      }
+      if (k in suite.injectedValues) {
+        throw new Error(`Injection key '${k}' has already been added.`);
+      }
+      
+      valuesMap[k] = true;
+      values.push({k, val: o[k]});
+      return o[k];
+    }));
   };
 
   v.assert = new Proxy(assrt, {
@@ -91,12 +165,12 @@ export const makeHookObj = function (hook: IHookObj, assertCount: IAssertObj,
     }
 
     planCalled = true;
-    if (hook.planCountExpected !== undefined) {
+    if (inject.planCountExpected !== undefined) {
       _suman.writeTestError(new Error(' => Suman warning => plan() called, even though plan was already passed as an option.').stack);
     }
 
     assert(Number.isInteger(num), 'Suman usage error => value passed to plan() is not an integer.');
-    hook.planCountExpected = v.planCountExpected = num;
+    inject.planCountExpected = v.planCountExpected = num;
   };
 
   v.confirm = function () {
