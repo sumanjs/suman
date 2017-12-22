5a18ce37a2c8ed444bef1dfb4c8310330ef180eb
Olegzandr Denman <alex@oresoftware.com>
2017-12-21 21:39:05 -0800

    pdev:set

--- lib/test-suite-helpers/t-proto-inject.d.ts
+++ lib/test-suite-helpers/t-proto-inject.d.ts
@@ -1,3 +1,3 @@
-import { IAssertObj, IHandleError, IHookObj } from "suman-types/dts/test-suite";
+import { IAssertObj, IHandleError, IHookObj, IHookParam } from "suman-types/dts/test-suite";
 import { ITestSuite } from 'suman-types/dts/test-suite';
-export declare const makeInjectParam: (inject: IHookObj, assertCount: IAssertObj, suite: ITestSuite, values: any[], handleError: IHandleError, fini: Function) => any;
+export declare const makeInjectParam: (inject: IHookObj, assertCount: IAssertObj, suite: ITestSuite, values: any[], handleError: IHandleError, fini: Function) => IHookParam;
