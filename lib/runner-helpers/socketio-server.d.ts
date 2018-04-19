eae0f68a145ae4784f5c4526f9e368249e6939da
Olegzandr VD <alex@oresoftware.com>
2018-04-19 15:13:49 -0700

    pdev:set

--- lib/runner-helpers/socketio-server.d.ts
+++ lib/runner-helpers/socketio-server.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="socket.io" />
+import * as SocketServer from 'socket.io';
 export declare const initializeSocketServer: (cb: Function) => void;
-export declare const getSocketServer: () => SocketIO.Server;
+export declare const getSocketServer: () => SocketServer.Server;
