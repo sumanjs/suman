6e88269df6a69c96e29c3aa442f047d216bed250
Olegzandr VD <alex@oresoftware.com>
2018-05-18 21:49:39 -0700

    pdev:set

--- scripts/find-local-suman-executable.js
+++ scripts/find-local-suman-executable.js
@@ -5,11 +5,10 @@ const path = require("path");
 const fs = require("fs");
 const cwd = process.cwd();
 const down = [];
-let found = false;
-let exec;
+let exec, found = false;
 const execNameIndex = process.argv.indexOf('--exec-name');
 if (execNameIndex < 0) {
-    exec = 'suman/cli.js';
+    exec = 'suman/dist/cli.js';
 }
 else {
     exec = '.bin/' + process.argv[execNameIndex + 1];
@@ -20,17 +19,12 @@ try {
 catch (err) {
 }
 const debugLogPath = path.resolve(process.env.HOME + '/.suman/suman-debug.log');
-fs.writeFileSync(debugLogPath, '\n\n', { flag: 'a' });
-fs.writeFileSync(debugLogPath, ' => Date run => ' + new Date().toISOString(), { flag: 'a' });
-fs.writeFileSync(debugLogPath, ' => Running find-local-suman-executable.\n', { flag: 'a' });
-fs.writeFileSync(debugLogPath, ' => cwd => ' + cwd, { flag: 'a' });
 let p, cd;
 function stat(p) {
     try {
         return fs.statSync(p).isFile();
     }
     catch (err) {
-        fs.writeFileSync(debugLogPath, '\n => stat error => ' + (err.stack || err), { flag: 'a' });
         if (!String(err.stack || err).match(/ENOENT: no such file or directory/i)) {
             throw err;
         }
@@ -40,11 +34,9 @@ function stat(p) {
 while (true) {
     cd = path.resolve(cwd + down.join(''));
     if (String(cd) === String(path.sep)) {
-        fs.writeFileSync(debugLogPath, '\n\n => Fail, (we went down to root "/") => cd => ' + cd, { flag: 'a' });
         break;
     }
     p = path.resolve(cd + '/node_modules/' + exec);
-    fs.writeFileSync(debugLogPath, '\n Searching for suman executable at this path => ' + p, { flag: 'a' });
     if (stat(p)) {
         found = true;
         break;
@@ -52,11 +44,9 @@ while (true) {
     down.push('/../');
 }
 if (found) {
-    fs.writeFileSync(debugLogPath, '\n Found => ' + p, { flag: 'a' });
     console.log(p);
     process.exit(0);
 }
 else {
-    fs.writeFileSync(debugLogPath, '\n * ! Not found * => ' + p, { flag: 'a' });
     process.exit(1);
 }
