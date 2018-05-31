0b2bd76c99fe5b083df85ebc6f410ff9881971d9
Olegzandr Denman <alex@oresoftware.com>
2018-05-30 20:30:23 -0700

    pdev:set

--- dev-scripts/travis/before_install.sh
+++ dev-scripts/travis/before_install.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+
+npm install -g typescript
+
+npm install\
+    "@types/async" \
+    "@types/chai"\
+    "@types/core-js" \
+    "@types/lodash"\
+    "@types/node"\
+    "@types/semver"\
+    "@types/socket.io"\
+    "@types/socket.io-client"
+
+
+tsc || echo "whatever";
