693f521cba3148e94cbdf859f3da5480534f9986
Olegzandr Denman <alex@oresoftware.com>
2018-05-31 01:56:27 -0700

    pdev:set

--- .docker.r2g/config.js
+++ .docker.r2g/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 if (!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')) {
-  throw new Error(chalk.magenta('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute path.'));
+  throw new Error('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute path.');
 }
 
 exports.default = {
--- dev-scripts/travis/install.sh
+++ dev-scripts/travis/install.sh
@@ -17,9 +17,9 @@
 #npm link suman;
 
 
- npm install -g typescript@2.8.3
- tsc || echo "whatevs"
- npm install
- npm link -f
- npm link suman
+npm install -g typescript@2.8.3
+tsc || echo "whatevs"
+npm install
+npm link -f
+npm link suman
 
