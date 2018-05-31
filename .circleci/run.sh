814ff410dd3ae40673af182f1b5055ca9794aa1f
Olegzandr Denman <alex@oresoftware.com>
2018-05-31 13:54:53 -0700

    pdev:set

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 sudo npm install -g typescript@2.8.3
+
+npm install         \
+    "suman-types"   \
+    "@types/async"  \
+    "@types/chai"   \
+    "@types/core-js" \
+    "@types/lodash" \
+    "@types/node"  \
+    "@types/semver" \
+    "@types/socket.io" \
+    "@types/socket.io-client"
+
 sudo tsc || echo "whatevs"
 sudo npm install
 sudo npm link -f
--- dev-scripts/travis/install.sh
+++ dev-scripts/travis/install.sh
@@ -16,6 +16,16 @@
 #npm link -f;
 #npm link suman;
 
+npm install         \
+    "suman-types"   \
+    "@types/async"  \
+    "@types/chai"   \
+    "@types/core-js" \
+    "@types/lodash" \
+    "@types/node"  \
+    "@types/semver" \
+    "@types/socket.io" \
+    "@types/socket.io-client"
 
 npm install -g typescript@2.8.3
 tsc || echo "whatevs"
