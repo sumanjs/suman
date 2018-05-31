1b940611157d95fc5e4c1f1546e082f00bf5c27f
Olegzandr Denman <alex@oresoftware.com>
2018-05-30 22:18:05 -0700

    pdev:set

--- dev-scripts/travis/before_install.sh
+++ dev-scripts/travis/before_install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 
-npm install -g "typescript@2.8.3"
-
-npm install\
-    "@types/async" \
-    "@types/chai"\
-    "@types/core-js" \
-    "@types/lodash"\
-    "@types/node"\
-    "@types/semver"\
-    "@types/socket.io"\
-    "@types/socket.io-client"
-
-
-tsc || echo "whatever";
+#npm install -g "typescript@2.8.3"
+#
+#npm install\
+#    "@types/async" \
+#    "@types/chai"\
+#    "@types/core-js" \
+#    "@types/lodash"\
+#    "@types/node"\
+#    "@types/semver"\
+#    "@types/socket.io"\
+#    "@types/socket.io-client"
+#
+#
+#tsc || echo "whatever";
--- dev-scripts/travis/install.sh
+++ dev-scripts/travis/install.sh
@@ -2,18 +2,24 @@
 
 #set -e;
 
-which_istanbul="$(which istanbul)"
+#which_istanbul="$(which istanbul)"
+#
+#if [[ -z "$which_istanbul" ]]; then
+#    npm install -g istanbul --silent
+#fi
+#
+#rm -rf node_modules
+#npm cache verify # npm cache clean
+#
+#npm install --loglevel=warn ;
+#
+#npm link -f;
+#npm link suman;
 
-if [[ -z "$which_istanbul" ]]; then
-    npm install -g istanbul --silent
-fi
 
-rm -rf node_modules
-npm cache verify # npm cache clean
-
-npm install --loglevel=warn ;
-
-npm link -f;
-npm link suman;
-
+sudo npm install -g typescript@2.8.3
+sudo tsc || echo "whatevs"
+sudo npm install
+sudo npm link -f
+sudo npm link suman
 
