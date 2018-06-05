de10f361b79c265552ac5bce7cc93941003ff1b1
Olegzandr Denman <alex@oresoftware.com>
2018-06-05 02:07:49 -0700

    pdev:set

--- src/test-suite-params/base.ts
+++ src/test-suite-params/base.ts
@@ -46,7 +46,7 @@ const slice = Array.prototype.slice;
 const notCallbackOrientedError = 'You have fired a callback for a test case or hook that was not callback oriented.';
 
 export class ParamBase extends EE implements IHookOrTestCaseParam {
-  
+
   protected __timerObj: ITimerObj;
   protected __handle: Function;
   protected __shared: VamootProxy;
@@ -56,11 +56,11 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
   should: typeof chai.should;
   expect: typeof chai.expect;
   protected __tooLate: boolean;
-  
+
   constructor() {
     super();
   }
-  
+
   timeout(val: number) {
     this.__timerObj.timer && clearTimeout(this.__timerObj.timer);
     try {
@@ -69,11 +69,11 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
     catch (e) {
       return this.__handle(e);
     }
-    
+
     const amount = _suman.weAreDebugging ? 5000000 : val;
     this.__timerObj.timer = setTimeout(this.onTimeout.bind(this), amount) as any;
   }
-  
+
   done(err?: Error) {
     if (this && this.__handle) {
       this.__handle(new Error(notCallbackOrientedError));
@@ -82,7 +82,7 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
       throw new Error(notCallbackOrientedError)
     }
   }
-  
+
   fatal(err: IPseudoError) {
     if (!err) {
       err = new Error('t.fatal() was called by the developer, with a falsy first argument.');
@@ -94,28 +94,28 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
     err.sumanFatal = true;
     this.__handle(err);
   }
-  
+
   set(k: string, v: any) {
     if (arguments.length < 2) {
       throw new Error('Must pass both a key and value to "set" method.');
     }
     return this.__shared.set(k, v);
   }
-  
+
   get(k?: string) {
     if (arguments.length < 1) {
       return this.__shared.getAll();
     }
     return this.__shared.get(k);
   }
-  
+
   getValues(...args: Array<string>) {
     const self = this;
     return args.map(function (k) {
       return self.__shared.get(k);
     });
   }
-  
+
   getMap(...args: Array<string>) {
     const self = this;
     const ret = {} as any;
@@ -124,7 +124,7 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
     });
     return ret;
   }
-  
+
   wrap(fn: Function) {
     const self = this;
     return function () {
@@ -136,38 +136,40 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
       }
     }
   };
-  
+
   wrapFinal(fn: Function) {
     const self = this;
     return function () {
       try {
         fn.apply(this, arguments);
-        self.__fini(null);
       }
       catch (e) {
-        self.__handle(e, false);
+        return self.__handle(e, false);
       }
+
+      self.__fini(null);
     }
   }
-  
+
   final(fn: Function, ctx?: any) {
     try {
       fn.call(ctx || null);
-      this.__fini(null);
     }
     catch (e) {
-      this.__handle(e, false);
+      return this.__handle(e, false);
     }
+
+    this.__fini(null);
   }
-  
+
   log(...args: Array<string>) {
     console.log(` [ '${this.desc || 'unknown'}' ] `, ...args);
   }
-  
+
   slow() {
     this.timeout(30000);
   }
-  
+
   wrapFinalErrorFirst(fn: Function) {
     const self = this;
     return function (err: Error) {
@@ -176,17 +178,18 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
       }
       try {
         fn.apply(this, slice.call(arguments, 1));
-        self.__fini(null);
       }
       catch (e) {
-        self.__handle(e, false);
+        return self.__handle(e, false);
       }
+
+      self.__fini(null);
     }
   }
-  
+
   wrapErrorFirst(fn: Function) {
     const self = this;
-    return function (err: IPseudoError) {
+    return function (err: any) {
       if (err) {
         return self.__handle(err, false);
       }
@@ -199,29 +202,29 @@ export class ParamBase extends EE implements IHookOrTestCaseParam {
       }
     }
   }
-  
-  handleAssertions(fn: Function) {
+
+  handleAssertions(fn: Function, ctx?: any) {
     try {
-      return fn.call(null);
+      return fn.call(ctx || null);
     }
     catch (e) {
       return this.__handle(e);
     }
   }
-  
-  handlePossibleError(err: Error | IPseudoError) {
+
+  handlePossibleError(err: any) {
     err ? this.__handle(err) : this.__fini(null)
   }
-  
-  handleNonCallbackMode(err: IPseudoError) {
+
+  handleNonCallbackMode(err: any) {
     err = err ? ('Also, you have this error => ' + err.stack || err) : '';
     this.__handle(new Error('Callback mode for this test-case/hook is not enabled, use .cb to enabled it.\n' + err));
   }
-  
+
   throw(str: any) {
     this.__handle(str instanceof Error ? str : new Error(str));
   }
-  
+
 }
 
 export interface ParamBase {
@@ -257,13 +260,13 @@ const expectCtx: any = {
 };
 
 const expct = <Partial<AssertStatic>> function () {
-  
+
   const ctx = expectCtx.val;
-  
+
   if (!ctx) {
     throw new Error('Suman implementation error => expect context is not defined.');
   }
-  
+
   try {
     return chai.expect.apply(chai.expect, arguments);
   }
@@ -274,17 +277,17 @@ const expct = <Partial<AssertStatic>> function () {
 
 const expectProxy = new Proxy(expct, {
   get: function (target, prop) {
-    
+
     if (typeof prop === 'symbol') {
       return Reflect.get.apply(Reflect, arguments);
     }
-    
+
     const ctx = expectCtx.val;
-    
+
     if (!ctx) {
       throw new Error('Suman implementation error => assert context is not defined.');
     }
-    
+
     if (!(prop in chai.expect)) {
       try {
         return Reflect.get.apply(Reflect, arguments);
@@ -295,7 +298,7 @@ const expectProxy = new Proxy(expct, {
         );
       }
     }
-    
+
     return function () {
       try {
         return (chai.expect as any)[prop].apply(chai.expect, arguments);
@@ -315,13 +318,13 @@ Object.defineProperty(proto, 'expect', {
 });
 
 const assrt = <Partial<AssertStatic>> function () {
-  
+
   const ctx = assertCtx.val;
-  
+
   if (!ctx) {
     throw new Error('Suman implementation error => assert context is not defined.');
   }
-  
+
   try {
     return chai.assert.apply(chai.assert, arguments);
   }
@@ -332,17 +335,17 @@ const assrt = <Partial<AssertStatic>> function () {
 
 const assertProxy = new Proxy(assrt, {
   get: function (target, prop) {
-    
+
     if (typeof prop === 'symbol') {
       return Reflect.get.apply(Reflect, arguments);
     }
-    
+
     const ctx = assertCtx.val;
-    
+
     if (!ctx) {
       throw new Error('Suman implementation error => assert context is not defined.');
     }
-    
+
     if (!(prop in chai.assert)) {
       try {
         return Reflect.get.apply(Reflect, arguments);
@@ -353,7 +356,7 @@ const assertProxy = new Proxy(assrt, {
         );
       }
     }
-    
+
     return function () {
       try {
         return (chai.assert as any)[prop].apply(chai.assert, arguments);
