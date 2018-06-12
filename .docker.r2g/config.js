8f1da79d60a1b37e3bdd1b04484f33ecde04fff0
Olegzandr Denman <alex@oresoftware.com>
2018-06-11 22:31:04 -0700

    pdev:set

--- .docker.r2g/config.js
+++ .docker.r2g/config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 
 if (!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')) {
@@ -5,19 +7,39 @@ if (!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')) {
 }
 
 exports.default = {
-  
+
   searchRoot: path.resolve(process.env.MY_DOCKER_R2G_SEARCH_ROOT),
-  
+
   // the following packages will be installed in the Docker container using this pattern:
   // npm install /r2g_shared_dir/Users/you/
-  
+
   tests: '',
-  
+
   packages: {
-    
     'residence': true,
-    'vamoot': true
-    
+    'vamoot': true,
+    'proxy-mcproxy': true,
+    'prepend-transform': true,
+    'log-prepend': true,
+    'pragmatik': true,
+    'frankenstop': true,
+    'poolio': true,
+    'freeze-existing-props': true,
+    'json-stdio': true,
+    'suman-r': true,
+    'suman-types': true,
+    'suman-browser': true,
+    'suman-watch': true,
+    'suman-shell': true,
+    'suman-utils': true,
+    'suman-events': true,
+    'suman-daemon': true,
+    'suman-browser-polyfills': true,
+    'suman-watch-plugins': true,
+    'suman-run-plugins': true,
+    'suman-transform-plugins': true,
+    'suman-reporters': true,
+    'suman-interactive': true
   }
-  
+
 };
