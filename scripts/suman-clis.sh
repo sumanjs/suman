2cbf20868aba1e439af10e315e9fc657564897b8
Olegzandr Denman <alex@oresoftware.com>
2018-06-29 00:06:19 -0700

    pdev:set

--- scripts/suman-clis.sh
+++ scripts/suman-clis.sh
@@ -42,11 +42,13 @@ handle_global_suman() {
 }
 
 
- suman(){
+suman(){
 
     if test ${#} -eq 0; then
+
         echo "[suman] using suman-shell instead of suman executable.";
         suman-shell "$@"; # note we should have no arguments so passing "$@" to suman-shell should be pointless
+
     else
 
         suman_inspect="no";
@@ -68,10 +70,12 @@ handle_global_suman() {
         NEW_PATH="${PATH}":"$HOME/.suman/global/node_modules/.bin";
 
         if [[ "${SUMAN_FORCE_GLOBAL}" == "yes" || -z "$LOCAL_SUMAN" ]]; then
+
             echo "[suman] => No local Suman executable could be found, given the present working directory => $PWD";
             echo "[suman] => Warning...attempting to run a globally installed version of Suman...";
             local -a node_exec_args=( );
             handle_global_suman node_exec_args "$@";
+
         else
             NODE_PATH="${NEW_NODE_PATH}" PATH="${NEW_PATH}" node "$LOCAL_SUMAN" "$@";
         fi
